859c780c76e5c5d2b82a905e13276eaedaa99f8d
RADJI LAMINE <lamine.radji@etu.unistra.fr>
2025-10-28 00:06:13 +0100

    implement GraphBuilder module with graph construction and distance function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "osm_reader.h"
+#include "graph_builder.h"
+#include <iostream>
 
 int main() {
-    OSMReader reader("../data/strasbourg.osm.pbf");
-    reader.read();
-    reader.printSummary();
-    return 0;
-}
+   // 1. Lecture du fichier OSM
+   OSMReader reader("data/strasbourg.osm.pbf");
+   reader.read();
+   reader.printSummary();
 
+   // 2. Construction du graphe à partir des données lues
+   GraphBuilder builder(reader.nodes, reader.ways);
+   builder.buildGraph();
+   builder.printSummary();
+
+   return 0;
+}
